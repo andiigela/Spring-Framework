0e1db5fd2700a02f265db4b4af16d6d83e15a178
Andi Gela <andi.gela@xponentl.ai>
2025-05-10 00:28:11 +0200

    Spring-01-Core-IoC: Create Beans and showing outputs from beans

--- spring-01-core-ioc/Spring-01-Core-IoC/src/main/java/com/andi/bean_annotation/CydeoApp.java
+++ spring-01-core-ioc/Spring-01-Core-IoC/src/main/java/com/andi/bean_annotation/CydeoApp.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class CydeoApp {
     public static void main(String[] args) {
-        ApplicationContext container = new AnnotationConfigApplicationContext();
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class, ConfigAny.class);
+        FullTimeMentor ft = container.getBean(FullTimeMentor.class);
+        ft.createAccount();
+        
+        String str = container.getBean(String.class);
+        System.out.println(str);
 
     }
 }
