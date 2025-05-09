7dbbcbc034ce6902ed7e3367d0b8716331c3d22c
Andi Gela <andi.gela@xponentl.ai>
2025-05-10 00:02:03 +0200

    spring-01-core-ioc: Create container with Application Context

---
+++ spring-01-core-ioc/Spring-01-Core-IoC/src/main/java/com/andi/bean_annotation/FullTimeMentor.java
@@ -0,0 +1,7 @@
+package com.andi.bean_annotation;
+
+public class FullTimeMentor {
+    public void createAccount(){
+        System.out.println("Full Mime Mentor is created!");
+    }
+}
---
+++ spring-01-core-ioc/Spring-01-Core-IoC/src/test/java/com/andi/spring01coreioc/Spring01CoreIoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andi.spring01coreioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring01CoreIoCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
