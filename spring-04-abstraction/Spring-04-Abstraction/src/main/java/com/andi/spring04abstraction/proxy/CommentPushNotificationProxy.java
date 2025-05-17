0ead201bceee09235928b391ab5ee3d1711662b3
Andi Gela <andi.gela@xponentl.ai>
2025-05-18 00:02:03 +0200

    When to use Qualify and Primary annotations

--- spring-04-abstraction/Spring-04-Abstraction/src/main/java/com/andi/spring04abstraction/proxy/CommentPushNotificationProxy.java
+++ spring-04-abstraction/Spring-04-Abstraction/src/main/java/com/andi/spring04abstraction/proxy/CommentPushNotificationProxy.java
@@ -1,15 +1,13 @@
 package com.andi.spring04abstraction.proxy;
 import com.andi.spring04abstraction.model.Comment;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
-@Qualifier("EMAIL") // same as Bean(name="")
-public class EmailCommentNotificationProxy implements CommentNotificationProxy {
+@Qualifier("PUSH") // same as Bean(name="")
+public class CommentPushNotificationProxy implements CommentNotificationProxy {
     @Override
     public void sendComment(Comment comment) {
-        System.out.println("Sending notification for comment: " + comment.getText());
+        System.out.println("Sending push notification for comment: " + comment.getText());
     }
 }
--- spring-04-abstraction/Spring-04-Abstraction/src/main/java/com/andi/spring04abstraction/service/CommentService.java
+++ spring-04-abstraction/Spring-04-Abstraction/src/main/java/com/andi/spring04abstraction/service/CommentService.java
@@ -2,6 +2,7 @@ package com.andi.spring04abstraction.service;
 import com.andi.spring04abstraction.model.Comment;
 import com.andi.spring04abstraction.proxy.CommentNotificationProxy;
 import com.andi.spring04abstraction.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +10,8 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository,
+                          @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
