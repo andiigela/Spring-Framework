3eafafbe9153b2fc6bb03f732e3865e751b24a4b
Andi Gela <andi.gela@xponentl.ai>
2025-05-26 21:42:58 +0200

    Practice Thymeleaf and Model with MVC architectural pattern

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/andi/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.andi.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StudentController {
+    @RequestMapping("/welcome")
+    public String homePage(Model model) {
+        model.addAttribute("name", "Cydeo");
+        model.addAttribute("course", "MVC");
+        return "/student/welcome";
+    }
+
+}
