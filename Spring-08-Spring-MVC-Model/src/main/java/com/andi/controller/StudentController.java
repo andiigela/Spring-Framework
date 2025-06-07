c76370d2cc4d7ac452ea0d6cac448f1f7c60d41e
Andi Gela <andi.gela@xponentl.ai>
2025-06-07 17:40:49 +0200

    Working with Model in Spring MVC

--- Spring-08-Spring-MVC-Model/src/main/java/com/andi/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/andi/controller/StudentController.java
@@ -3,12 +3,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
     @RequestMapping("/welcome")
     public String homePage(Model model) {
         model.addAttribute("name", "Cydeo");
         model.addAttribute("course", "MVC");
+
+        String subject = "Collections";
+        model.addAttribute("subject", subject);
+
+        int studentId = new Random().nextInt(1000);
+        model.addAttribute("id", studentId);
+
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(10);
+        model.addAttribute("numbers", numbers);
         return "/student/welcome";
     }
 
